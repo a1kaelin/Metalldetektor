7fb43853ab29e93742bc089ed2e609009093b38e
Andrin <andrin.samuel.kaelin@gmail.com>
2018-09-15 15:37:21 +0200

    SensorEventListener Implementiert

--- app/src/main/java/com/example/andrin/metalldetektor/SensorDemoActivity.java
+++ app/src/main/java/com/example/andrin/metalldetektor/SensorDemoActivity.java
@@ -1,13 +1,31 @@
 package com.example.andrin.metalldetektor;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SensorDemoActivity extends AppCompatActivity {
+public class SensorDemoActivity extends AppCompatActivity implements SensorEventListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
