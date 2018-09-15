be3d9e95018abe01c52b411c3011d289826afb78
lucavanbrussel <luca.willem@gmail.com>
2018-09-15 15:49:15 +0200

    Erster Versuch Layout

--- app/src/main/java/com/example/andrin/metalldetektor/SensorDemoActivity.java
+++ app/src/main/java/com/example/andrin/metalldetektor/SensorDemoActivity.java
@@ -11,7 +11,7 @@ public class SensorDemoActivity extends AppCompatActivity implements SensorEvent
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activitysensordemo);
     }
 
     @Override
